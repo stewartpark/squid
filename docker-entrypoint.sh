226b56bd6d011735d65ca39f3374ab845fe0602d
Stewart Park <hello@stewartjpark.com>
2020-03-09 21:11:22 -0700

    Support disk cache

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-tail -vn 0 -F /var/log/squid/access.log /var/log/squid/cache.log &
+mkdir -p /var/cache/squid
+tail -vn 0 -F /var/log/squid/access.log /var/log/squid/cache.log /var/log/squid/store.log &
+squid -z -f /squid.conf -N || true
 squid -f /squid.conf -N
